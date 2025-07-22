b46bb9e13ab2077eaf73d138750e1a48c00efa7d
reg <reg@MacBook-Pro-mac.local>
2025-07-22 10:51:53 +0300

    22.07.25

--- barrier-plus/src/components/ContactsSection.js
+++ barrier-plus/src/components/ContactsSection.js
@@ -9,31 +9,18 @@ const ContactsSection = () => {
         href="/contacts"
         >
       <div className={styles.contactsContainer}>
-        <h2>OUR CONTACTS</h2>
+        <h2>НАШІ КОНТАКТИ</h2>
         <div className={styles.contactBlock}>
-          <h3>Department of support for private clients</h3>
-          <p>Phone: <a href="tel:+380500575960">+38 050 057-59-60</a></p>
-          <p>E-mail: <a href="mailto:network_shelter@ukr.net">network_shelter@ukr.net</a></p>
-        </div>
-        <div className={styles.contactBlock}>
-          <h3>Support department for corporate clients</h3>
-          <p>Phone: <a href="tel:+380632899886">+38 063 289-98-86</a></p>
-          <p>Phone: <a href="tel:+380674593946">+38 067 459-39-46</a></p>
-          <p>E-mail: <a href="mailto:shelterukraine.sale@ukr.net">shelterukraine.sale@ukr.net</a></p>
-          <p>E-mail: <a href="mailto:shelterukraine.sale2@ukr.net">shelterukraine.sale2@ukr.net</a></p>
-        </div>
-        <div className={styles.contactBlock}>
-          <h3>Main office</h3>
-          <p>Phone: <a href="tel:+380734220391">+38 073 422-03-91</a></p>
-          <p>E-mail: <a href="mailto:shelter.ukraine.23@gmail.com">shelter.ukraine.23@gmail.com</a></p>
-          <p>Address: м. Київ, вул. Бориспільська, 9</p>
+          <p>Телефон: <a href="tel:+380734220391">+38 0 (98) 955 04 64</a></p>
+          <p>Електронна пошта: <a href="mailto:barrierplusukraine@gmail.com">barrierplusukraine@gmail.com</a></p>
+          <p>Адрес: Київ, бульвар Вацлава Гавела, буд 8, корпус 9</p>
         </div>
       </div>
 
       <div className={styles.formContainer}>
-        <h3>ORDER A CALL</h3>
+        <h3>ЗАМОВТЕ ДЗВІНОК</h3>
         <form className={styles.contactForm}>
-          <label htmlFor="name">Your name</label>
+          <label htmlFor="name">Ваше ім`я</label>
           <input
             type="text"
             id="name"
@@ -41,7 +28,7 @@ const ContactsSection = () => {
             placeholder="Steve"
             required
           />
-          <label htmlFor="email">Your E-mail</label>
+          <label htmlFor="email">Ваша електронна адреса</label>
           <input
             type="email"
             id="email"
@@ -49,7 +36,7 @@ const ContactsSection = () => {
             placeholder="steve@mail.com"
             required
           />
-          <label htmlFor="phone">Phone number</label>
+          <label htmlFor="phone">Номер телефону</label>
           <input
             type="tel"
             id="phone"
@@ -57,7 +44,7 @@ const ContactsSection = () => {
             placeholder="+38(0"
             required
           />
-          <button type="submit">Order</button>
+          <button type="submit">Замовлення</button>
         </form>
       </div>
       </Link>
